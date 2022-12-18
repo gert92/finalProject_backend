09c5de98082b1a9311eb874ece3b4e8c30789b9c
gert92 <gertolen92@gmail.com>
2022-12-18 12:51:48 +0200

    first

---
+++ src/main/java/com/seg/SdaTravelAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.seg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaTravelAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaTravelAgencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seg/SdaTravelAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaTravelAgencyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
